ad4405d36832f4bb08ff253f27806c750bc148d0
ayushydv1805 <ayushyadav0000018@gmail.com>
2026-09-20 22:20:35 +0530

    Add 404 route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,20 +8,20 @@ import WaitingRoom from "./pages/WaitingRoom";
 import Game from "./pages/Game";
 import RoundResult from "./pages/RoundResult";
 import Leaderboard from "./pages/Leaderboard";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-
         <Route path="/" element={<Home />} />
-
         <Route path="/create-room" element={<CreateRoom />} />
-
         <Route path="/join-room" element={<JoinRoom />} />
-<Route path="/game" element={<Game />} />
-<Route path="/round-result" element={<RoundResult />} />
-<Route path="/waiting-room" element={<WaitingRoom />} />
-<Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/round-result" element={<RoundResult />} />
+        <Route path="/waiting-room" element={<WaitingRoom />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
