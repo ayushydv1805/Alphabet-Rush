51a48e8dc943e484a3e376ac6e7a032decd166e0
Ayush Yadav <ayushyadav0000018@gmail.com>
2026-09-20 21:27:59 +0530

    Prepare server for deployment

--- server/index.js
+++ server/index.js
@@ -727,10 +727,8 @@ startRound(roomCode, 1);
 // START SERVER
 // =========================
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
-  console.log(
-    `Alphabet Rush server running on http://localhost:${PORT}`
-  );
+  console.log(`Server running on port ${PORT}`);
 });
